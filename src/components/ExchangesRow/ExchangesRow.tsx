b8d34f935bdb29251335bc458a9a7beff9a728fd
K0nfigMDA <iamjustadreamer@yandex.ru>
2023-02-08 12:17:43 +0300

    feat: add exchanges layouts

--- src/components/ExchangesRow/ExchangesRow.tsx
+++ src/components/ExchangesRow/ExchangesRow.tsx
@@ -1,20 +1,30 @@
 import { IExchange } from '../../interfaces/exchanges';
 import './ExchangesRow.scss';
 
-
 interface IExchangesRow {
-	data: IExchange
+   data: IExchange;
 }
 
-export const ExchangesRow = ({data}:IExchangesRow) => {
+export const ExchangesRow = ({ data }: IExchangesRow) => {
    return (
       <tr>
          <td className="exchanges__number">{data.trust_score_rank}</td>
-         <td className="exchanges__img-name">
-					<img src={data.image} alt="exchange__image" />
-					<span>{data.name}</span>
-				 </td>
-         <td className="exchanges__trust">{data.trust_score}</td>
+         <td>
+					<div className="exchanges__img-name">
+            <img
+               className="exchanges__img"
+               src={data.image}
+               alt="exchange__image"
+            />
+            <span className="exchanges__name">{data.name}</span>
+						</div>
+         </td>
+         <td className="exchanges__trust">
+            <div className="trust__progres-line">
+               <div className="trust__inner-progress-line"></div>
+            </div>
+            {data.trust_score}
+         </td>
          <td className="exchanges__24h-normalized">
             {data.trade_volume_24h_btc_normalized}
          </td>
