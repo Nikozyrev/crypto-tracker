8ce29c8a1a7a76127fd64d25d387a7c2f2c42186
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-02-13 13:00:37 +0300

    feat: add converter selects

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -1,50 +1,83 @@
 import { useEffect, useState } from 'react';
+import { Autocomplete, TextField } from '@mui/material';
 import { priceFormatter } from '../../helpers/price';
 import { useAppSelector } from '../../hooks/redux';
 import { useGetCoinPriceQuery, useGetSupportedVsCurrenciesQuery } from '../../store/coingecko/coingecko.api';
+import { ICoinSearched } from '../../interfaces/coinSearched';
+import { CoinSelect } from '../CoinSelect';
 import './Converter.scss';
 
 export const Converter = () => {
   const {currency: globalCurrency} = useAppSelector(state => state.currency);
   const {data: currencies} = useGetSupportedVsCurrenciesQuery();
-  const [coin, setCoin] = useState('bitcoin');
+  const [coin, setCoin] = useState<ICoinSearched>({
+    id: 'bitcoin',
+    name: 'Bitcoin',
+    api_symbol: '',
+    symbol: 'BTC',
+    market_cap_rank: 1,
+    thumb: '',
+    large: '',
+  });
   const [currency, setCurrency] = useState(globalCurrency);
-  const {data} = useGetCoinPriceQuery({ids: coin, vs_currencies: currencies?.join(',') || currency});
+  const {currentData} = useGetCoinPriceQuery({ids: coin.id, vs_currencies: currencies?.join(',') || currency}, {
+    skip: !currencies
+  });
   const [price, setPrice] = useState(0);
   const [updateTime, setUpdateTime] = useState<Date | null>(null);
   const [quantity, setQuantity] = useState<number | null>(1);
   
   useEffect(() => {
-    if (data) {
-      setPrice(data[coin][currency]);
-      setUpdateTime(new Date(data[coin].last_updated_at * 1000));
+    if (currentData) {
+      setPrice(currentData[coin.id][currency]);
+      setUpdateTime(new Date(currentData[coin.id].last_updated_at * 1000));
     }
-  }, [data])
+  }, [currentData, coin, currency])
 
   const changeQuantityHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value 
-      ? Number(e.target.value) < 0
-        ? null
-        : Number(e.target.value)
-      : null;
-    setQuantity(value);
+    const value = e.target.value;
+    const numberValue = Number(value);
+    const newValue = numberValue >= 0 ? numberValue : null;
+    setQuantity(newValue);
   }
 
   return (
-    <div>
-      <input 
-        type="number"
-        name="converter_quantity"
-        id="converter_quantity" 
-        value={quantity ?? ''}
-        onChange={changeQuantityHandler}
-      />
-      {!!quantity &&
-      <>
-        <div>{priceFormatter(coin)(quantity)} = {priceFormatter(currency)(quantity * price)}</div>
-        <div>Last updated at {updateTime?.toLocaleTimeString()}</div>      
-      </>
-      }
+    <div className='converter_container'>
+      <div className='converter_form'>
+        <TextField 
+          type='number'
+          inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+          label='Enter Amount' 
+          placeholder='e.g. 1.00'
+          value={quantity ?? ''}
+          onChange={changeQuantityHandler}
+          sx={{
+            backgroundColor: 'white'
+          }}
+        />
+        <CoinSelect coin={coin} setCoin={setCoin}/>
+        <Autocomplete 
+          options={currencies || []}
+          renderInput={(params) => <TextField {...params} label="
+          Select Currency" />}
+          value={currency}
+          onChange={(_, v) => setCurrency(v || currency)}
+          clearOnEscape
+          placeholder='e.g. USD'
+          fullWidth
+          sx={{
+            backgroundColor: 'white'
+          }}
+        />
+      </div>
+      <div className="converter_result">
+        {!!quantity &&
+        <>
+          <div>{priceFormatter(coin.symbol)(quantity)} = {priceFormatter(currency)(quantity * price)}</div>
+          <div>Last updated at {updateTime?.toLocaleTimeString()}</div>      
+        </>
+        }
+      </div>
     </div>
   );
 }
