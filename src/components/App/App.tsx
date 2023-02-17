a7c568d016fab491c79ed662a092bb68688a873d
K0nfigMDA <iamjustadreamer@yandex.ru>
2023-02-17 15:20:05 +0300

    feat: add footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,27 @@
-
-import { Route, Routes } from "react-router-dom";
-import { ROUTES } from "../../constants/routes";
-import { MainPage } from "../../pages/MainPage";
-import { CoinsPage } from "../../pages/CoinsPage";
-import { ExchangesPage } from "../../pages/ExchangesPage";
-import { ConverterPage } from "../../pages/ConverterPage";
-import { NotFoundPage } from "../../pages/NotFoundPage";
-import { Header } from "../Header";
+import { Route, Routes } from 'react-router-dom';
+import { ROUTES } from '../../constants/routes';
+import { MainPage } from '../../pages/MainPage';
+import { CoinsPage } from '../../pages/CoinsPage';
+import { ExchangesPage } from '../../pages/ExchangesPage';
+import { ConverterPage } from '../../pages/ConverterPage';
+import { NotFoundPage } from '../../pages/NotFoundPage';
+import { Header } from '../Header';
+import { Footer } from '../Footer';
 
 export const App = () => {
-
-  return (
-    <div className="app">
-      <Header/>
-      <Routes>
-        <Route path={ROUTES.MAIN} element={ <MainPage/> } />
-        <Route path={`${ROUTES.COINS}/:id`} element={ <CoinsPage/> } />
-        <Route path={ROUTES.EXCHANGES} element={ <ExchangesPage/> } />
-        <Route path={ROUTES.CONVERTER} element={ <ConverterPage/> } />
-        <Route path={ROUTES.NOT_FOUND} element={ <NotFoundPage/> } />
-      </Routes>
-    </div>
-  );
-}
-
+   return (
+      <div className="app">
+         <div className="_container">
+            <Header />
+            <Routes>
+               <Route path={ROUTES.MAIN} element={<MainPage />} />
+               <Route path={`${ROUTES.COINS}/:id`} element={<CoinsPage />} />
+               <Route path={ROUTES.EXCHANGES} element={<ExchangesPage />} />
+               <Route path={ROUTES.CONVERTER} element={<ConverterPage />} />
+               <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage />} />
+            </Routes>
+         </div>
+				 <Footer />
+      </div>
+   );
+};
