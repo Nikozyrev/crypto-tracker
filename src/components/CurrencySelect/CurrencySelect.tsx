2c5540a60dcad9acaf4ec81470e530b5c3084167
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-02-26 12:05:41 +0300

    feat: add currency select modal with grouping

--- src/components/CurrencySelect/CurrencySelect.tsx
+++ src/components/CurrencySelect/CurrencySelect.tsx
@@ -1,6 +1,6 @@
 import { Box, Modal } from '@mui/material';
 import { useState } from 'react';
-import { CURRENCIES } from '../../constants/currencies';
+import { CURRENCIES, CURR_CATEGORIES, CURR_DESCRIPTION } from '../../constants/currencies';
 import { useAppActions, useAppSelector } from '../../hooks/redux';
 import { useGetSupportedVsCurrenciesQuery } from '../../store/coingecko/coingecko.api';
 import './CurrencySelect.scss';
@@ -10,11 +10,11 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
+  borderRadius: '1.5rem',
   boxShadow: 24,
-  p: 4,
-  outline: 0,
+  p: 3,
+  outline: 0,  
 };
 
 export const CurrencySelect = () => {
@@ -22,6 +22,10 @@ export const CurrencySelect = () => {
   const { changeCurrency } = useAppActions();
   const { data } = useGetSupportedVsCurrenciesQuery();
   const [dropdown, setDropdown] = useState(false);
+  const currencyCategories = data?.reduce((acc: CURR_CATEGORIES[], el): CURR_CATEGORIES[] => {
+    const elCategory = CURR_DESCRIPTION[el].category;
+    return acc.includes(elCategory) ? acc : [...acc, elCategory];
+  }, []);
 
   const showDropdown = () => {
     setDropdown(!dropdown);
@@ -42,17 +46,28 @@ export const CurrencySelect = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <h3>Select Currency</h3>
-          <ul className="currency-select">
-          {data?.map(el => 
-              <li
-                className={`currency-option` + `${el === currency ? ' active' : ''}`}
-                key={el} 
-                value={el} 
-                onClick={() => changeCurrencyHandler(el)}
-              >{el.toUpperCase()}</li>  
-            )}
-          </ul>
+          <div className='currency-select'>
+            <h2 style={{marginBottom: '10px'}}>Select Currency</h2>
+            {currencyCategories?.map(category => 
+            <div
+              key={category}
+              className="currency-category"
+            >
+              <h4>{category}</h4>
+              <ul className='currency-options'>
+                {data?.filter(el => CURR_DESCRIPTION[el].category === category).map(el => 
+                    <li
+                      className={`currency-option` + `${el === currency ? ' active' : ''}`}
+                      key={el}
+                      onClick={() => changeCurrencyHandler(el)}
+                    >
+                      <div className='currency-name'>{CURR_DESCRIPTION[el].name}</div>
+                      <div className='currency-symbol'>{el.toUpperCase()}</div>
+                    </li>
+                  )}
+              </ul>
+            </div>)}
+          </div>
         </Box>
       </Modal>
     </div>
